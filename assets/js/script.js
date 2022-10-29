5f4e31f38b9706357c9d8e16647204944330cc6a
Pedro0604 <pedrospadari0604@gmail.com>
2022-10-29 20:30:14 -0300

    barra buscadora

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-f = ((a) => a + 3)
+let busqueda = document.getElementById("busqueda")
+let divBusqueda = document.getElementById("divBusqueda")
+let barraBuscadora = document.getElementById("barraBuscadora");
 
-console.log( f(1) )
+
+function createInput(){
+    let input=document.createElement("input")
+    input.id = "barraBuscadora"
+    input.placeholder = "Búsqueda"
+    divBusqueda.appendChild(input)
+}
+
+busqueda.addEventListener("mouseover",function(e){
+    busqueda.hidden=true;
+    barraBuscadora.hidden=false
+})
+
+barraBuscadora.addEventListener("mouseout",function(e){
+    barraBuscadora.hidden=true;
+    busqueda.hidden=false;
+})
+
