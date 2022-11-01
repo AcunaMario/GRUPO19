75e21bcd498b29ab91ee6de7be1ce1b30a7d2a6c
Pedro0604 <pedrospadari0604@gmail.com>
2022-10-31 23:45:27 -0300

    paginaRecetas

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,35 +5,78 @@ const options = {
         'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
     }
 };
-const url = 'https://calorieninjas.p.rapidapi.com/v1/nutrition?query='
+const urlNutritionAPI = 'https://calorieninjas.p.rapidapi.com/v1/nutrition?query='
 
-const fetchApp = (ingredientes) => {
-    return fetch(url + ingredientes, options)
+const fetchAppNutrition = (ingredientes) => {
+    return fetch(urlNutritionAPI + ingredientes, options)
         .then(response => response.json())
         .catch(err => console.error(err));
+    // calories
+    // carbohydrates_total_g
+    // cholesterol_mg
+    // fat_saturated_g
+    // fat_total_g
+    // fiber_g
+    // name
+    // potassium_mg
+    // protein_g
+    // serving_size_g
+    // sodium_mg
+    // sugar_g
 }
 
-// calories
-// carbohydrates_total_g
-// cholesterol_mg
-// fat_saturated_g
-// fat_total_g
-// fiber_g
-// name
-// potassium_mg
-// protein_g
-// serving_size_g
-// sodium_mg
-// sugar_g
+const urlRecipesAPI = 'https://api.sampleapis.com/recipes/recipes'
+
+const fetchAppRecipes = () => {
+    fetch(urlRecipesAPI)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            for (recipe of response) {
+                const $tableBody = document.getElementById('tableBody');
+                let tr = document.createElement("tr")
+                let td = document.createElement("td")
+                td.innerHTML = recipe.title;
+                tr.append(td)
+                td = document.createElement("td")
+                td.innerHTML = recipe.calories;
+                tr.append(td)
+                td = document.createElement("td")
+                td.innerHTML = recipe.fat;
+                tr.append(td)
+                td = document.createElement("td")
+                td.innerHTML = recipe.carbohydrate;
+                tr.append(td)
+                td = document.createElement("td")
+                let a = document.createElement("a")
+                a.href = recipe.url;
+                a.target = "blank"
+                a.innerHTML = recipe.title;
+                td.append(a)
+                tr.append(td)
+                $tableBody.append(tr)
+            }
+        })
+        .catch(err => console.error(err));
+    // calories
+    // carbohydrate
+    // categoryname
+    // cholesterol
+    // cooktime
+    // cuisine
+    // description
+    // fat
+    // fiber
+    // title
+}
 
 const $lupa = document.getElementById("lupa");
 const $searchBar = document.getElementById("barraBuscadora");
 
-
-$lupa.addEventListener("click", async function (e) {
+async function appNutrition(e) {
     $lupa.ariaBusy = true
     const ingredientes = $searchBar.value
-    const { items } = await fetchApp(ingredientes)
+    const { items } = await fetchAppNutrition(ingredientes)
     let aux = ""
     for (let item of items) {
         aux += "Alimento: " + item.name + "\n";
@@ -42,23 +85,30 @@ $lupa.addEventListener("click", async function (e) {
         aux += "Proteínas: " + item.protein_g + "g\n\n";
     }
     console.log(aux);
-})
+}
 
-$searchBar.addEventListener("keydown", async function (e) {
+$lupa.addEventListener("click", appNutrition);
+
+$searchBar.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
-        $lupa.ariaBusy = true
-        const ingredientes = $searchBar.value
-        const { items } = await fetchApp(ingredientes)
-        let aux = ""
-        for (let item of items) {
-            aux += "Alimento: " + item.name + "\n";
-            aux += "Cantidad: " + item.serving_size_g + "g\n";
-            aux += "Calorias: " + item.calories + "cal\n";
-            aux += "Proteínas: " + item.protein_g + "g\n\n";
-        }
-        console.log(aux);
+        appNutrition();
     }
-})
+});
 
 
-// API RECETAS: https://api.sampleapis.com/recipes/recipes
+async function appRecipes() {
+    const infoApp = await fetchAppRecipes();
+    console.log(infoApp);
+
+}
+
+
+$lupa.addEventListener("click", appRecipes);
+
+$searchBar.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        appRecipes();
+    }
+});
+
+fetchAppRecipes();
